b9479e1e9a3dc448a8ac3aff56bf5ba19766f0b1
beatrizGJ <beatrizgarcia.jim@gmail.com>
2020-01-22 20:18:50 +0100

    add storage

---
+++ src/js/storage.js
@@ -0,0 +1,24 @@
+import Cookies from 'js-cookie'
+
+const lStorage = {
+    setItem: (key, value) => localStorage.setItem(key, value),
+    getItem: key => localStorage.getItem(key),
+  };
+  
+  const cookieStorage = {
+    setItem: (key, value) => Cookies.set(key, value),
+    getItem: key => Cookies.get(key),
+  };
+  
+  const storage = (type = 'lStorage') => {
+    const types = {
+      lStorage,
+      cookieStorage,
+    };
+    if (typeof(Storage) !== 'undefined') {
+      return types[type];
+    }
+    return type['cookieStorage'];
+  };
+  
+  export default storage;
