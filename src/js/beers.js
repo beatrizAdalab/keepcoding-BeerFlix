d222ba55c5405e691d13acf2b837e1042e1eff9c
Beatriz García <beatrizgarcia@Yuanfeng-iMac.local>
2020-01-23 17:25:15 +0100

    add style card

--- src/js/beers.js
+++ src/js/beers.js
@@ -6,21 +6,23 @@ const templateBeer = beer => {
         //created template html
     return `
     <div class="col mb-4">
-    <div class="card">
-    <div class="row no-gutters">
-    <div class="col-md-4">
-      <img src="..." class="card-img" alt="...">
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row no-gutters">
+                    <div class="col-md-3">
+                        <img src="..." class="card-img" alt="...">
+                    </div>
+                
+                    <div class="col-md-9">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
     </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-      </div>
-    </div>
-  </div>
-    </div>
-  </div>`;   
+    `;   
 }
 
 const renderBeers = (element, items) => {
